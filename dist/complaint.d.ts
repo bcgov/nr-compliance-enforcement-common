551a6443c39220f48e5df34fca174e50856b90ac
Ryan Rondeau <ryan_rondeau@live.ca>
2025-04-04 10:24:38 -0700

    Update complaint.d.ts

--- dist/complaint.d.ts
+++ dist/complaint.d.ts
@@ -1,29 +1,30 @@
 export interface BaseComplaint {
-    id: string;
-    details: string;
-    name: string;
-    address: string;
-    email: string;
-    phone1: string;
-    phone2: string;
-    phone3: string;
-    location: {
-        type: string;
-        coordinates: Array<number>;
-    };
-    locationSummary: string;
-    locationDetail: string;
-    reportedBy?: string;
-    reportedByOther: string;
-    incidentDateTime?: Date;
-    createdBy: string;
-    updatedBy: string;
-    webeocId: string;
-    isPrivacyRequested: string;
-    status: string;
-    ownedBy: string;
-    reportedOn: Date;
-    updatedOn: Date;
-    complaintMethodReceivedCode: string;
-    referenceNumber: string;
+  id: string;
+  details: string;
+  name: string;
+  address: string;
+  email: string;
+  phone1: string;
+  phone2: string;
+  phone3: string;
+  location: {
+    type: string;
+    coordinates: Array<number>;
+  };
+  locationSummary: string;
+  locationDetail: string;
+  reportedBy?: string;
+  reportedByOther: string;
+  incidentDateTime?: Date;
+  createdBy: string;
+  updatedBy: string;
+  webeocId: string;
+  isPrivacyRequested: string;
+  status: string;
+  ownedBy: string;
+  reportedOn: Date;
+  updatedOn: Date;
+  complaintMethodReceivedCode: string;
+  referenceNumber: string;
+  parkGuid: string;
 }
